b808bdd36afb181954f63f7a077102daa77c3cf9
Edwin van der Graaf <edwin@digitpaint.nl>
2015-06-17 00:16:35 +0200

    Update middleware to use env["roger.project"] for project variables

--- lib/roger_sassc/middleware.rb
+++ lib/roger_sassc/middleware.rb
@@ -3,10 +3,17 @@ require "roger/resolver"
 require "rack"
 
 module RogerSassc
-  # Rack Middleware for Roger sass
+  # Rack Middleware for Roger Sassc
+  #
+  # This middleware transforms a given url scss -> css. It does this
+  # by checking if the .css can be resolved to .scss file, if so it
+  # compiles (with the help of libsass) the scss.
   class Middleware
+    attr_writer :resolver
+
     def initialize(app, options = {})
       @app = app
+
       defaults = {
         load_paths: RogerSassc.load_paths
       }
@@ -15,11 +22,13 @@ module RogerSassc
     end
 
     def call(env)
+      @project = env["roger.project"]
+
       url = ::Rack::Utils.unescape(env["PATH_INFO"].to_s).sub(%r{^/}, "")
 
+      # Convert the url to an absolute path,
+      # which is used to retrieve compile the scss
       if url.end_with?(".css") && scss_path = resolve_url(url)
-        # Convert the url to an absolute path,
-        # which is used to retrieve the asset from sprockets
         css = compile_scss(scss_path)
         respond(css)
       else
@@ -30,16 +39,21 @@ module RogerSassc
     private
 
     def resolve_url(url)
+      # A .css file is requested in the browser
       url = url[0..-4] + "scss"
+
       # Use the resolver to translate urls to file paths
       # returns nill when no file is found via url
-      resolver = Roger::Resolver.new(@app.project.html_path)
       resolver.url_to_path url, exact_match: true
     end
 
     def compile_scss(scss_path)
       engine = SassC::Engine.new(File.read(scss_path), @options)
       engine.render
+    end
+
+    def resolver
+      @resolver ||= Roger::Resolver.new(@project.html_path)
     end
 
     def respond(css)
--- test/middleware_test.rb
+++ test/middleware_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 require "./lib/roger_sassc/middleware"
 
 module RogerSassc
-  # Testing the Roger Test fucntionality
+  # Testing the middleware functionality
+  # which transforms on a request based .scss to .css
   class TestMiddleware < ::Test::Unit::TestCase
     include FixtureHelper
 
@@ -22,16 +23,15 @@ module RogerSassc
     end
 
     def test_doesn_t_compile_scss_when_no_scss_file
-      @middleware.stubs(:resolve_url).returns nil
+      @middleware.resolver = mock_resolver("not-excisting.scss", nil)
       assert_equal @request.get("/not-excisting.css").body, "YAM"
     end
 
     def test_respons_with_css
       path = fixture_path "general.scss"
       expected_css = fixture "output.css"
-
-      @middleware.stubs(:resolve_url).returns path
-      assert_equal @request.get("test/fixtures/general.css").body, expected_css
+      @middleware.resolver = mock_resolver("test/fixtures/general.scss", path)
+      assert_equal @request.get("/test/fixtures/general.css").body, expected_css
     end
 
     def test_source_maps
@@ -40,6 +40,14 @@ module RogerSassc
 
     def test_write_to_fs
       pend "To implement"
+    end
+
+    private
+
+    def mock_resolver(input_url, file_path)
+      resolver = mock
+      resolver.expects(:url_to_path).with(equals(input_url), anything).returns(file_path)
+      resolver
     end
   end
 end
