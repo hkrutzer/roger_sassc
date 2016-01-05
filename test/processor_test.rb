7ba87488f8e37aace5042529c5a20582e5b667f7
Edwin van der Graaf <edwin@digitpaint.nl>
2016-01-05 16:06:18 +0100

    Use mockRelease over custom mock
    Still mocking out get files to get easier access to force
    file usage within the test.

--- test/processor_test.rb
+++ test/processor_test.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 require "fileutils"
+require "roger/testing/mock_release"
 require "./lib/roger_sassc/processor"
 
 module RogerSassc
@@ -7,39 +8,40 @@ module RogerSassc
   class TestProcessor < ::Test::Unit::TestCase
     include FixtureHelper
 
-    TEST_OUTPUT = "../../tmp/test/fixtures/"
-
     def setup
-      @processor = Processor.new {}
-      @processor.stubs(:build_path).returns("./")
+      @release = Roger::Testing::MockRelease.new
+      @release.project.construct.directory "build" do |dir|
+        @test_output = dir.directory "test/fixtures/"
+        FileUtils.cp_r fixture_path("."), @test_output
+      end
 
-      # Copy fixtures files to fixtures/out
-      output_path = fixture_path(TEST_OUTPUT)
-      FileUtils.rm_rf output_path
-      FileUtils.mkdir_p output_path
-      FileUtils.cp_r fixture_path("."), output_path
+      @processor = Processor.new
+    end
+
+    def teardown
+      @release.destroy
     end
 
     def test_processor_can_be_called
       assert_respond_to(@processor, :call)
     end
 
-    # Meh :(
     def test_call_processor
       files = [
-        fixture_path(TEST_OUTPUT + "general.scss"),
-        fixture_path(TEST_OUTPUT + "src/_variables.scss")
+        (@test_output + "general.scss").to_s,
+        (@test_output + "src/_variables.scss").to_s
       ]
 
-      release = release_mock(files)
+      stub_get_files(files)
+
       expected_css = fixture "output.css"
-      @processor.call release
+      @processor.call @release
 
       # File is created
-      assert_path_exist fixture_path(TEST_OUTPUT + "general.css")
+      assert_path_exist @test_output + "general.css"
 
       # And matches earlier output
-      assert_equal fixture(TEST_OUTPUT + "general.css"), expected_css
+      assert_equal File.read(@test_output + "general.css"), expected_css
 
       # Check clean up
       assert_path_not_exist files[0]
@@ -48,23 +50,22 @@ module RogerSassc
 
     def test_processor_raises_on_compilation_errors
       files = [
-        fixture_path(TEST_OUTPUT + "raise.scss")
+        (@test_output + "raise.scss").to_s
       ]
 
-      release = release_mock(files)
+      stub_get_files(files)
 
       assert_raise SassC::SyntaxError do
-        @processor.call release
+        @processor.call @release
       end
     end
 
     private
 
-    def release_mock(files)
-      release = mock(get_files: files,
-                     log: ->(_s, _m) {})
-
-      release
+    # Stub is used so we can controll what files
+    # are ran through the processor.
+    def stub_get_files(files)
+      @release.stubs(:get_files).returns(files)
     end
   end
 end
